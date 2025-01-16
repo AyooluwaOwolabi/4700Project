3727f251b0d3792eb271c33a66b6e2c1fc2a00b3
Ayo Owolabi <ayooluwaowolabi@ad.doe.carleton.ca>
2025-01-16 13:47:36 -0500

    Milestone 1 Code

--- TLM.m
+++ TLM.m
@@ -125,8 +125,9 @@ for i = 2:Nt
         plot(time*1e12,real(InputL), 'r'); hold on
         plot(time*1e12,real(OutputR), 'g');
         plot(time*1e12,real(InputR), 'b'); 
-        plot(time*1e12,real(OutputL), 'm'); 
-        xlim(YL)
+        plot(time*1e12,real(OutputL), 'm');
+        xlim([0,Nt*dt*1e12])
+        ylim(YL)
         xlabel('time(ps)')
         ylabel('0')
         legend('Left Input', 'Right Output', 'Right Input', 'Left Output', 'Location', 'east')
