e5a55c4ddda0c2a81358a1adaebf333105c90cbf
Ayo Owolabi <ayooluwaowolabi@ad.doe.carleton.ca>
2025-02-06 13:11:12 -0500

    Milestone 3 Code

--- M3/TLM_M3.m
+++ M3/TLM_M3.m
@@ -1,131 +1,87 @@
-% Name: Ayooluwa OWOLABI and Std#: 101237575 
-% ELEC 4700A  Project Milestone3
-%Set default plot appearance and visualization settings
+% ELEC 4700A - Project Milestone 3
+% Name: Ayooluwa Owolabi and Student#: 101237575
+% Adding a grating to the waveguide and analyzing wave propagation
+
+% Set default plot appearance and visualization settings 
 set(0, 'defaultaxesfontsize',20) % Sets the default font size of the axes
 set(0, 'DefaultFigureWindowStyle','docked') % Sets the default Figure Window style to be docked 
 set(0, 'DefaultLineLineWidth',2) % Sets the Default Line Width; i.e. from 0 to 2 
 set(0, 'DefaultaxesLineWidth',2) % Sets the Default Line Width of the axes
-
 % set(0, 'DefaultFigureWindowStyle','docked') - Duplicated code 
 
-% Initializing the fundamental constants 
-c_c = 299792458; % TWM speed of light (m/s)
-c_eps_0 = 8.8542149e-12; % vacuum permittivity (F/m)
-c_eps_0_cm = c_eps_0/100; % Vacuum permittivity measured in F/cm
-c_mu_0 = 1/c_eps_0/c_c^2; % Permittivity of free space
-c_q = 1.60217653e-19; % The value of an elementary charge
-c_hb = 1.05457266913e-34;     % Dirac Constant
-c_h = c_hb*2*pi; % Planck constant 
 
-% Define and Initialize the input parameters for the left and right sources
-% InputParasL.E0= 1e5;  % Amplitude
-% InputParasL.we= 0;    % Angular Frequency
-% InputParasL.t0= 2e-12;  % Time offset 
-% InputParasL.wg= 5e-13;  % Gaussian width 
-% InputParasL.phi= 0; % phase 
-% InputParasR= 0; % No input is implemented for the right source  
+% Initializing Fundamental constants
+c_c = 299792458; % Speed of light (m/s)
+n_g = 3.5; % Group index of the medium
+vg = c_c/n_g*1e2; % TWM Group velocity (cm/s)
+L = 1000e-6*1e2; % Medium length (cm)
+Lambda = 1550e-9; % Wavelength of light
+Nz = 500; % Spatial points
+dz = L/(Nz-1); % Spatial step
+dt = dz/vg; % Time step
+Nt = floor(2*Nz); % Time steps
+time = linspace(0, Nt*dt, Nt);
 
-% Define and Initialize the input parameters for the left and right sources
-% (Modulated Gaussian)
+% Input pulse parameters (Modulated Gaussian)
 InputParasL.E0= 1e5;  % Amplitude
-InputParasL.we= 5e12;    % Angular Frequency
+InputParasL.we= 0;    % Angular Frequency
 InputParasL.t0= 2e-12;  % Time offset 
 InputParasL.wg= 5e-13;  % Gaussian width 
-InputParasL.phi= 0; % phase 
-InputParasR= struct(); % No input is implemented for the right source 
+InputParasL.phi= 0; % phase
+InputParasR = 0;
 
-InputParasR.E0= 0;  % Amplitude
-InputParasR.we= 0;    % Angular Frequency
-InputParasR.t0= 0;  % Time offset 
-InputParasR.wg= 1;  % Gaussian width (Avoiding zero-division)
-InputParasR.phi= 0; % phase 
+% Define spatial grid
+z = linspace(0, L, Nz).';
 
+% Define kappa (grating strength)
+kappa0 = 100;
+kappa = kappa0 * ones(size(z));
+kappa(z < 0.3*L | z > 0.7*L) = 0; % Grating in central region
 
-% The source function with modulation
-function E = SourceFct_M3(t, InputParas)
-E = InputParas.E0 * exp(-((t-InputParas.t0.^2) / (2 * InputParas.wg^2)) ).* exp(1j * (InputParas.we * t + InputParas.phi));
-end 
-
-% Adding the reflection coefficients for mirrors 
-RL = 0.9i; % The left mirror reflection coefficient 
-RR = 0.9i; % The right mirror reflection coefficient 
-
-% Adding more input parameters for the gain/loss
-beta_r = 80;
-beta_i =8;
-
-% Define material properties and simulation parameters
-n_g = 3.5; % Group index of the medium
-vg = c_c/n_g*1e2; % TWM group velocity (cm/s)
-Lambda = 1550e-9; % Wavelength of light (m)
-
-% Define and Initialize the simulation parameters
-% plotN = 10; % How frequent the plots are updated; Output speed 
-plotN = 10; % How often the plots are updated has been increased to 50; making the resulting plot output to appear faster
-L = 1000e-6*1e2; % Length of the medium
-XL = [0,L]; % Range for the z-axis
-YL = [-2*InputParasL.E0, 5*InputParasL.E0]; % Range for the y-axis
-
-Nz = 500; % Number of points of the z-axis
-dz = L/(Nz-1); % The spatial step size
-dt = dz/vg; % Time step size 
-fsync = dt*vg/dz; % Field sync factor 
-
-Nt = floor(2*Nz); % Number of Time steps 
-tmax = Nt*dt; % Max simulation time
-t_L = dt*Nz;   % time to travel the length of the medium
-
-% Initialize the spatial (z-axis) and temporal arrays 
-z = linspace(0,L,Nz).';  %Nz points, Nz-1 segments 
-time = nan(1,Nt); 
-InputL = nan(1,Nt);
-InputR = nan(1,Nt);
-OutputL = nan(1,Nt);
-OutputR = nan(1,Nt);
-
-Ef = zeros(size(z)); % Forward propagating electric field 
-Er = zeros(size(z)); % Reverse propagating electric field 
-
-%d Define grating kappa 
-kappa_0 = 100;
-kappa = kappa_0 * ones(size(z));
-kappa(z < 0.4*L) = 0; 
-kappa(z < 0.6*L) = 0;
-
-% Plotting kappa 
-figure; 
-plot(z, kappa);
+% Plot kappa vs z
+figure;
+plot(z, kappa, 'k', 'LineWidth', 2);
 xlabel('z (cm)');
-ylabel('kappa');
-title('The plotting of kappa as a function of z');
+ylabel('\kappa');
+title('Grating Profile');
 
+% Initialize fields
+Ef = zeros(size(z));
+Er = zeros(size(z));
+
+% Define Gaussian-modulated input source
+SourceFct = @(t, InputParas) InputParas.E0 * exp(-((t-InputParas.t0).^2) / (2 * InputParas.wg^2)) .* exp(1j * (InputParas.we * t + InputParas.phi));
+
+% Reflection coefficients
+RL = 0i;
+RR = 0i;
+
+% Adding more input parameters and simulation parameters (Gain and Detuning factors)
+beta_r = 0; % Gain Factor (γ)
+beta_i = 0; % Detuning factor ​
+% Define and Initialize the simulation parameters 
+plotN = 50; % How often the plots are updated (output speed)
+XL = [0, L];
+YL = [-2*InputParasL.E0, 5*InputParasL.E0]; % Range of the y-axis 
+fsync = dt*vg/dz; % Field sync factor
+tmax = Nt*dt; % Max simulation time 
+t_L = dt*Nz; % time to travel the lenght of the medium 
+
+% Initialize input and output signals
+InputL = zeros(1, Nt); 
+InputR = zeros(1, Nt);
+OutputL = zeros(1, Nt); 
+OutputR = zeros(1, Nt);
+
+% Ef = zeros(size(z)); % Forward propagating electric field 
+% Er = zeros(size(z)); % Reverse propagating electric field 
 
 % Modified Ef and Modified Er 
 beta = ones(size(z))*(beta_r+1i*beta_i);
 exp_det = exp(-1i*dz*beta);
 
-% Time stepping loop
-for i = 2:Nt
-    Ef(1) = InputL(i) + RL * Er(1);
-    Er(Nz) = InputR(i) + RR * Ef(Nz);
-
-    OutputR(i) = Ef(Nz) * (1-RR);
-    OutputL(i) = Er(1) * (1-RL);
-
-    t = dt*(i-1);
-    time(i) = t;
-
-    InputL(i) = SourceFct_M3(t, InputParasL);
-    InputR(i) = SourceFct_M3(t, InputParasR);
-
-    Ef(2:Nz) = fsync * exp_det(1:Nz-1) .* Ef(1:Nz-1) + 1i * dz * kappa(2:Nz) .* Er(2:Nz);
-    Er(1:Nz-1) = fsync * exp_det(2:Nz) .* Er(2:Nz) + 1i * dz * kappa(1:Nz-1) .* Ef(1:Nz-1);
-end
-
-
-%----The codes that have been removed to focus on working on miletsone 3----
-% Ef1 = @SourceFct; % Left source function
-% ErN = @SourceFct; % Right source function
+% Ef1 = @SourceFct_M3; % Left source function
+% ErN = @SourceFct_M3; % Right source function
 % 
 % % The intial time step
 % t = 0; 
@@ -160,109 +116,110 @@ end
 % ylabel('E')
 % 
 % hold off
-% 
-% % Define loop for the time stepping of the field propagation 
-% for i = 2:Nt
-%     % Modified Ef and Er to implement the reflections 
-%     Ef(1) = InputL(i) + RL*Er(1);
-%     Er(Nz) = InputR(i) + RR*Ef(Nz);
-% 
-%     % Modified Output for both the right and left of the mirror 
-%     OutputR(i) = Ef(Nz)*(1-RR);
-%     OutputL(i) = Er(1)*(1-RL);
-% 
-%     % The time is updated 
-%     t = dt*(i-1);
-%     time(i) = t;
-% 
-%     InputL(i) = Ef1(t,InputParasL); % Left input field
-%     InputR(i) = ErN(t, 0); % Right input field
-% 
-%     % The boundary conditions
-%     Ef(1) = InputL(i);
-%     Er(Nz) = InputR(i);
-% 
-%     % Modifying Ef and Er to implement mirrors 
-%     Ef(1) = InputL(i) + RL*Er(1);
-%     Er(Nz) = InputR(i) + RR*Ef(Nz);
-% 
-%     % The forward and backward propagations of the field
-%     % Ef(2:Nz) = fsync*Ef(1:Nz-1); % The forward propagation for the field  
-%     % Er(1:Nz-1) = fsync*Er(2:Nz); % The reverse propagation (backwards) for the field 
-% 
-%     % Updating the forward and backward propagations of the field 
-%     Ef(2:Nz) = fsync*exp_det(1:Nz-1).*Ef(1:Nz-1);
-%     Er(1:Nz-1) = fsync*exp_det(2:Nz).*Er(2:Nz);
-% 
-%     % Updating the output so it reflect on the field 
-%     OutputR(i) = Ef(Nz);
-%     OutputL(i) = Er(1);
-% 
-%     % The modified Output implementing the mirrors 
-%     OutputR(i) = Ef(Nz)*(1-RR);
-%     OutputL(i) = Er(1)*(1-RL);
-% 
-%     % The periodic plotting of the field 
-%     if mod(i,plotN) == 0
-%         subplot(3,1,1)
-%         plot(z*10000,real(Ef), 'r'); hold on
-%         plot(z*10000,imag(Ef), 'r--'); hold off
-%         xlim(XL*1e4)
-%         ylim(YL)
-%         xlabel('z(\mum)')
-%         ylabel('E_f')
-%         legend('\Re','\Im')
-%         hold off 
-%         subplot(3,1,2)
-%         plot(z*10000,real(Er), 'b'); hold on
-%         plot(z*10000,imag(Er), 'b--'); hold off
-%         xlim(XL*1e4)
-%         ylim(YL)
-%         xlabel('z(\mum)')
-%         ylabel('E_r')
-%         legend('\Re', '\Im')
-% 
-%         hold off
-%         subplot(3,1,3);
-%         plot(time*1e12,real(InputL), 'r'); hold on
-%         plot(time*1e12,real(OutputR), 'g');
-%         plot(time*1e12,real(InputR), 'b'); 
-%         plot(time*1e12,real(OutputL), 'm');
-%         xlim([0,Nt*dt*1e12])
-%         ylim(YL)
-%         xlabel('time(ps)')
-%         ylabel('0')
-%         legend('Left Input', 'Right Output', 'Right Input', 'Left Output', 'Location', 'east')
-%         hold off
-%         pause(0.01) % The pause considered to allow for the plotting animation
-%     end 
-% end 
-% 
-% 
-% % Fourier Transform of OutputR 
-% fftOutput = fftshift(fft(OutputR));
-% omega = fftshift(wspace_M2(time));
-% 
-% % Plot the time-domain signal
-% figure; 
-% plot(time * 1e12, real(OutputR), 'r');
-% xlabel('Time (ps)');
-% ylabel('Output Signal');
-% title('Time Domain Output vs Time');
-% 
-% figure;
-% tiledlayout(2,1);
-% 
-% % Plot magnitude of FFT (Top plot)
-% nexttile;
-% plot(omega, abs(fftOutput), 'm');
-% xlabel('Frequency (rad/s)');
-% ylabel('|FFT(OutputR)|');
-% title('Magnitude of FFT Output');
-% 
-% % Plot phase of FFT (using unwrap) (Bottom plot)
-% nexttile;
-% plot(omega, unwrap(angle(fftOutput)), 'r');
-% xlabel('Frequency (rad/s)');
-% ylabel('Phase (radians)');
-% title('Phase of FFT Output');
+
+% Initialize time vector
+time_vec = zeros(1, Nt);  % Allocate memory to avoid dynamic resizing
+
+% Define loop for the time stepping of the field propagation 
+for i = 2:Nt
+    time_vec(i) = dt * (i-1);  % Update time at each step
+    t = time_vec(i);
+    InputL(i) = SourceFct(t, InputParasL);
+
+    % Modified Ef and Er to implement the reflections 
+    Ef(1) = InputL(i) + RL*Er(1);
+    Er(Nz) = RR * Ef(Nz);
+
+    % % The time is updated 
+    % t = dt*(i-1);
+    % time(i) = t;
+
+    % Stores the previous Ef values 
+    Ef_prev = Ef;
+    Er_prev = Er;
+    
+    % Update forward and backward fields with grating terms
+    Ef(2:Nz) = fsync*exp_det(1:Nz-1).*Ef(1:Nz-1) + 1i * dz * kappa(2:Nz) .* Er(2:Nz);
+    Er(1:Nz-1) = fsync*exp_det(2:Nz).*Er(2:Nz) + 1i * dz * kappa(1:Nz-1) .* Ef_prev(1:Nz-1);
+
+    % The modified Output implementing the mirrors 
+    OutputR(i) = Ef(Nz)*(1-RR);
+    OutputL(i) = Er(1)*(1-RL);
+
+    % The periodic plotting of the field 
+    if mod(i,plotN) == 0
+        subplot(3,1,1)
+        plot(z*10000,real(Ef), 'r'); hold on
+        plot(z*10000,imag(Ef), 'r--'); hold off
+        xlim(XL*1e4)
+        ylim(YL)
+        xlabel('z(\mum)')
+        ylabel('E_f')
+        legend('\Re','\Im')
+        hold off 
+        subplot(3,1,2)
+        plot(z*10000,real(Er), 'b'); hold on
+        plot(z*10000,imag(Er), 'b--'); hold off
+        xlim(XL*1e4)
+        ylim(YL)
+        xlabel('z(\mum)')
+        ylabel('E_r')
+        legend('\Re', '\Im')
+
+        hold off
+        subplot(3,1,3);
+        plot(time*1e12,real(InputL), 'r'); hold on
+        plot(time*1e12,real(OutputR), 'g');
+        plot(time*1e12,real(InputR), 'b'); 
+        plot(time*1e12,real(OutputL), 'm');
+        xlim([0,Nt*dt*1e12])
+        ylim(YL)
+        xlabel('time(ps)')
+        ylabel('0')
+        legend('Left Input', 'Right Output', 'Right Input', 'Left Output', 'Location', 'east')
+        hold off
+        pause(0.01) % The pause considered to allow for the plotting animation
+    end 
+end 
+
+% Compute and plot spectral content
+fftOutputR = fftshift(fft(OutputR));
+fftOutputL = fftshift(fft(OutputL));
+fftInputL = fftshift(fft(InputL));
+omega = fftshift(wspace_M3(time));
+
+% Plot time-domain output
+figure;
+plot(time_vec * 1e12, real(OutputR), 'r');
+xlabel('Time (ps)'); 
+ylabel('Output Signal');
+title('Time Domain Output vs Time');
+
+figure;
+tiledlayout(3,1);
+
+% Plot FFT Magnitude (Right Output)
+% This gives a high pass filter (transmission) because the low frequencies are attenuated
+% allowing the high frequencies to match and pass through
+nexttile;
+plot(omega, abs(fftOutputR), 'r');
+xlabel('Frequency (rad/s)');
+ylabel('|FFT(OutputR)|');
+title('Magnitude of FFT Output (Right)');
+
+% Plot FFT Magnitude (Left Output)
+% This gives a low pass filter (reflection) because the high frequencies
+% are attenuated allowing the low frequencies to match and pass through
+% attenuated 
+nexttile;
+plot(omega, abs(fftOutputL), 'b');
+xlabel('Frequency (rad/s)');
+ylabel('|FFT(OutputL)|');
+title('Magnitude of FFT Output (Left)');
+
+% Plot FFt Magnitude (Gaussian)
+nexttile; 
+plot(omega, abs(fftInputL), 'm');
+xlabel('Frequency (rad/s)');
+ylabel('|InputL|');
+title('Magnitude of InputL')
