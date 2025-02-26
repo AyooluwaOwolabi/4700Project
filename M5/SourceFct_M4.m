339bedff6719ba9eea506a94d3c0467cbaea5c39
Ayo Owolabi <ayooluwaowolabi@ad.doe.carleton.ca>
2025-02-26 13:30:00 -0500

    Milestone 5 Code

---
+++ M5/SourceFct_M4.m
@@ -0,0 +1,15 @@
+function E = SourceFct_M4(t,InputParas)
+
+if isfield(InputParas, 'rep')
+    n = floor(t/InputParas.rep);
+    t = t-n*InputParas.rep;
+end 
+
+if ~isstruct(InputParas)
+    E = InputParas;
+else 
+    E = InputParas.E0*exp(-(t-InputParas.t0)^2/InputParas.wg^2)*exp(1i*(InputParas.we*t + InputParas.phi));
+
+end 
+
+end 
